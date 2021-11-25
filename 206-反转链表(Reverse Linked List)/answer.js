d3f416c14b1587e29edceaaefbe3110445a6ab96
LudensLee <Ludens.lee42@gmail.com>
2021-11-25 23:53:40 +0800

    add: 链表-leetcode206-反转链表题目和迭代的解法
    add: 链表-leetcode206-反转链表题目和迭代的解法

---
+++ 206-反转链表(Reverse Linked List)/answer.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 迭代解法
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+   // 头节点的前一个节点为null，故定义变量prev为null
+   let prev = null
+   // 定义变量curr保存当前节点的引用值，初始化为传进来的head头节点 
+   let curr = head
+   //当curr不为空时，循环
+   while(curr){
+        // 定义变量next保存当前节点未翻转前的next
+        const next = curr.next;
+        // 将当前节点的next指向上一个节点，则变量prev中的值 
+        curr.next = prev;
+        // 由于翻转之后，当前的节点为下一个节点的next，故将变量prev的值改为当前的节点
+        prev = curr
+        //将当前的节点指向下个节点
+        curr = next
+    }
+    // 以输入[1，2，3]为例，那每次循环以prev为头节点的链表为
+    // 第一次循环 [1，null]
+    // 第二次循环 [2，1，null]
+    // 第三次循环 [3，2，1，null]
+    // 由于3的next为null 故退出循环体，链表反转结束，return当前的头节点prev
+    return prev
+};
---
+++ 206-反转链表(Reverse Linked List)/question.md
@@ -0,0 +1,30 @@
+## 206 反转链表
+* 给你单链表的头节点 `head` ，请你反转链表，并返回反转后的链表。
+>输入：head = [1,2,3,4,5]
+>输出：[5,4,3,2,1]
+>输入：head = [1,2]
+>输出：[2,1]
+>输入：head = []
+>输出：[]
+
+* 提示：
+    1. 链表中节点的数目范围是 `[0, 5000]`
+    2. `-5000 <= Node.val <= 5000`
+ 
+* 进阶：链表可以选用`迭代`或`递归`方式完成反转。
+
+
+## 206 Reverse Linked List
+* Given the `head` of a singly linked list, reverse the list, and return the reversed list.
+>Input：head = [1,2,3,4,5]
+>Output：[5,4,3,2,1]
+>Input：head = [1,2]
+>Output：[2,1]
+>Input：head = []
+>Output：[]
+
+* Constraints：
+    1. The number of nodes in the list is the range  `[0, 5000]`
+    2. `-5000 <= Node.val <= 5000`
+ 
+* Follow up：A linked list can be reversed either `iteratively` or `recursively`. 
